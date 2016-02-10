1a856be1cb153b50798a308baa8e7bfb4906d4e7
David Raleigh <davidraleigh@gmail.com>
2016-02-10 11:11:30 -0800

    step one

--- boston_housing.py
+++ boston_housing.py
@@ -29,14 +29,37 @@ def explore_city_data(city_data):
     ### Step 1. YOUR CODE GOES HERE ###
     ###################################
 
+    # - CRIM     per capita crime rate by town
+    # - ZN       proportion of residential land zoned for lots over 25,000 sq.ft.
+    # - INDUS    proportion of non-retail business acres per town
+    # - CHAS     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+    # - NOX      nitric oxides concentration (parts per 10 million)
+    # - RM       average number of rooms per dwelling
+    # - AGE      proportion of owner-occupied units built prior to 1940
+    # - DIS      weighted distances to five Boston employment centres
+    # - RAD      index of accessibility to radial highways
+    # - TAX      full-value property-tax rate per $10,000
+    # - PTRATIO  pupil-teacher ratio by town
+    # - B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+    # - LSTAT    % lower status of the population
+    # - MEDV     Median value of owner-occupied homes in $1000's
+
+
     # Please calculate the following values using the Numpy library
     # Size of data (number of houses)?
+    print len(city_data.target)
     # Number of features?
+    print len(city_data.feature_names)
     # Minimum price?
+    print min(city_data.target)
     # Maximum price?
+    print max(city_data.target)
     # Calculate mean price?
+    print np.mean(city_data.target)
     # Calculate median price?
+    print np.median(city_data.target)
     # Calculate standard deviation?
+    print np.std(city_data.target)
 
 
 def performance_metric(label, prediction):
@@ -59,7 +82,10 @@ def split_data(city_data):
     ###################################
     ### Step 3. YOUR CODE GOES HERE ###
     ###################################
-
+    X_train = 0
+    y_train = 0
+    X_test = 0
+    y_test = 0
     return X_train, y_train, X_test, y_test
 
 
@@ -187,18 +213,18 @@ def main():
     explore_city_data(city_data)
 
     # Training/Test dataset split
-    X_train, y_train, X_test, y_test = split_data(city_data)
+    # X_train, y_train, X_test, y_test = split_data(city_data)
 
-    # Learning Curve Graphs
-    max_depths = [1,2,3,4,5,6,7,8,9,10]
-    for max_depth in max_depths:
-        learning_curve(max_depth, X_train, y_train, X_test, y_test)
+    # # Learning Curve Graphs
+    # max_depths = [1,2,3,4,5,6,7,8,9,10]
+    # for max_depth in max_depths:
+    #     learning_curve(max_depth, X_train, y_train, X_test, y_test)
 
-    # Model Complexity Graph
-    model_complexity(X_train, y_train, X_test, y_test)
+    # # Model Complexity Graph
+    # model_complexity(X_train, y_train, X_test, y_test)
 
-    # Tune and predict Model
-    fit_predict_model(city_data)
+    # # Tune and predict Model
+    # fit_predict_model(city_data)
 
 
 if __name__ == "__main__":
